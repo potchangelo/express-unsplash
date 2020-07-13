c73876d2e2ce182bca7de868294a7ed23f6f6164
Potchara Puttawanchai <potchangelo@gmail.com>
2020-07-13 19:52:55 +0700

    More seeder data

--- seeders/20200711085926-demo-photo.js
+++ seeders/20200711085926-demo-photo.js
@@ -14,14 +14,46 @@ module.exports = {
         updatedAt: new Date("2020-07-20 09:30"),
         userUid: 'o4weapfvs4'
       },
-      
+      {
+        uid: 'yth45984o8',
+        width: 1534,
+        height: 2301,
+        creditUser: 'Kristian Egelund',
+        creditUserLink: 'https://unsplash.com/@kristianegelund?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText',
+        creditPhotoLink: 'https://unsplash.com/photos/FaD-Y5atnT4',
+        createdAt: new Date("2020-07-20 15:37"),
+        updatedAt: new Date("2020-07-20 15:37"),
+        userUid: 'o4weapfvs4'
+      },
+      {
+        uid: '45hfxg156w',
+        width: 1575,
+        height: 2100,
+        creditUser: 'Tom Juggins',
+        creditUserLink: 'https://unsplash.com/@tomjuggins_uk?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText',
+        creditPhotoLink: 'https://unsplash.com/photos/AWguZEkl5dg',
+        createdAt: new Date("2020-07-20 15:42"),
+        updatedAt: new Date("2020-07-20 15:42"),
+        userUid: 'o4weapfvs4'
+      },
+      {
+        uid: 'uyesth5614',
+        width: 1575,
+        height: 2104,
+        creditUser: 'Mihai Moisa',
+        creditUserLink: 'https://unsplash.com/@moisamihai092?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText',
+        creditPhotoLink: 'https://unsplash.com/photos/8HDWBobi7MI',
+        createdAt: new Date("2020-07-20 15:45"),
+        updatedAt: new Date("2020-07-20 15:45"),
+        userUid: 'o4weapfvs4'
+      },
     ]);
   },
 
   down: async (queryInterface, Sequelize) => {
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete('Photos', {
-      uid: { [Op.in]: ['5jtcj402ca'] }
+      uid: { [Op.in]: ['5jtcj402ca', 'yth45984o8', '45hfxg156w', 'uyesth5614'] }
     });
   }
 };
