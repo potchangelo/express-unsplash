cf4e0a7ba849387d6398ca23649891b751781395
Potchara Puttawanchai <potchangelo@gmail.com>
2022-10-06 16:20:55 +0700

    Prepare starting point for v.2

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 require('dotenv').config();
-require('newrelic');
 const express = require('express');
 const cors = require('cors');
-const { sequelize } = require('./models');
-const { photoController, userController, topicController, searchController } = require('./controllers');
+// const { sequelize } = require('./models');
+// const { photoController, userController, topicController, searchController } = require('./controllers');
 
 const app = express();
 const port = Number(process.env.PORT || process.env.APP_PORT);
@@ -11,30 +10,23 @@ const port = Number(process.env.PORT || process.env.APP_PORT);
 app.use(cors());
 
 app.get('/', async (req, res) => {
-  let message = 'Connection OK',
-    status = 200;
-  try {
-    await sequelize.authenticate();
-  } catch (error) {
-    console.error(error);
-    message = 'Connection Error';
-    status = 502;
-  }
-  res.status(status).send(`Express Unsplash-cloned API by Zinglecode (for educational purposes only), ${message}.`);
+  let statusMessage = 'Connection OK';
+  let status = 200;
+  res.status(status).send(`Express Unsplash-cloned API by Zinglecode (for educational purposes only), ${statusMessage}.`);
 });
 
-app.get('/photos', photoController.getPhotos);
-app.get('/photos/random', photoController.getRandomPhoto);
-app.get('/photos/:uid', photoController.getPhoto);
+// app.get('/photos', photoController.getPhotos);
+// app.get('/photos/random', photoController.getRandomPhoto);
+// app.get('/photos/:uid', photoController.getPhoto);
 
-app.get('/users', userController.getRandomUsers);
-app.get('/users/:username', userController.getUser);
+// app.get('/users', userController.getRandomUsers);
+// app.get('/users/:username', userController.getUser);
 
-app.get('/topics', topicController.getTopics);
-app.get('/topics/:uidOrSlug', topicController.getTopic);
+// app.get('/topics', topicController.getTopics);
+// app.get('/topics/:uidOrSlug', topicController.getTopic);
 
-app.get('/search/photos', searchController.searchPhotos);
-app.get('/search/users', searchController.searchUsers);
+// app.get('/search/photos', searchController.searchPhotos);
+// app.get('/search/users', searchController.searchUsers);
 
 app.listen(port, () => {
   console.log(`Express Unsplash-cloned API run on port ${port}!`);
