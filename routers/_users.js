c4e715ca22272152d9759788549e45344bd77b3b
Potchara Puttawanchai <potchangelo@gmail.com>
2022-10-14 16:22:07 +0700

    Easier random by shuffle latest users

--- routers/_users.js
+++ routers/_users.js
@@ -6,48 +6,13 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
   let users = [];
-
-  // Postgres random by TABLESAMPLE
-  // SYSTEM ...% from all rows
-  // BERNOULI ...
-  // SYSTEM_ROWS -> Must install Extension tsm_system_rows first
   try {
-    /**
-     * @type {(import('@prisma/client').User)[]}
-     */
-    const usersRaw = await prisma.$queryRaw`
-      SELECT "u".*,
-      "ua"."id" AS "ua_id", "ua"."small" AS "ua_small",
-      "ua"."medium" AS "ua_medium", "ua"."large" AS "ua_large",
-      "ua"."creditUser" AS "ua_creditUser", "ua"."creditUserLink" AS "ua_creditUserLink",
-      "ua"."creditPhotoLink" AS "ua_creditPhotoLink",
-      "ua"."createdAt" AS "ua_createdAt", "ua"."updatedAt" AS "ua_updatedAt"
-      FROM "User" AS "u" TABLESAMPLE SYSTEM_ROWS (3)
-      LEFT JOIN "UserAvatar" AS "ua" ON "u"."uid" = "ua"."userUid";
-    `;
-
-    // Reconstruct
-    users = usersRaw.map(userRaw => {
-      const {
-        ua_id, ua_small, ua_medium, ua_large,
-        ua_creditUser, ua_creditUserLink, ua_creditPhotoLink,
-        ua_createdAt, ua_updatedAt,
-        ...user
-      } = userRaw;
-
-      /**
-       * @type {import('@prisma/client').UserAvatar|null}
-       */
-      let avatar = null;
-      if (!!ua_id) {
-        avatar = {
-          id: ua_id, small: ua_small, medium: ua_medium, large: ua_large,
-          creditUser: ua_creditUser, creditUserLink: ua_creditUserLink, creditPhotoLink: ua_creditPhotoLink,
-          createdAt: ua_createdAt, updatedAt: ua_createdAt, userUid: user.uid
-        }
-      }
-      return { ...user, avatar };
+    users = await prisma.user.findMany({
+      include: { avatar: true },
+      orderBy: { id: 'desc' },
+      take: 3
     });
+    users.sort(() => Math.random() - 0.5);
   } catch (error) {
     console.error(error);
     return res.status(500).json({
