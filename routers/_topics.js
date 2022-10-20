846f244d75e3a6d7c0150e80f3e3dda0d8a3fed9
Potchara Puttawanchai <potchangelo@gmail.com>
2022-10-20 13:25:47 +0700

    Get topic by uid or slug

--- routers/_topics.js
+++ routers/_topics.js
@@ -1,4 +1,6 @@
+const { Prisma } = require('@prisma/client');
 const express = require('express');
+const prisma = require('../prisma/client');
 
 const router = express.Router();
 
@@ -7,7 +9,32 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:uidOrSlug', async (req, res) => {
-  res.send({ uidOrSlug: req.params.uidOrSlug });
+  const { uidOrSlug } = req.params;
+  let topic = null;
+  try {
+    topic = await prisma.topic.findFirstOrThrow({
+      where: {
+        OR: [
+          { uid: uidOrSlug },
+          { slug: uidOrSlug }
+        ]
+      },
+      include: { cover: true }
+    });
+  } catch (error) {
+    console.error(error);
+    if (error instanceof Prisma.NotFoundError) {
+      return res.status(404).json({
+        errorCode: 404,
+        errorMessage: 'User not found',
+      });
+    }
+    return res.status(500).json({
+      errorCode: 500,
+      errorMessage: 'Error on get user',
+    });
+  }
+  res.status(200).json({ topic });
 });
 
 module.exports = router;
